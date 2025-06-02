37fa707ff53472d1287bd9f08ca0883a643de389
bornovak <novakbor999@gmail.com>
2025-06-02 17:06:23 +0200

    Added event tab

--- Frontend-Web/src/Header.jsx
+++ Frontend-Web/src/Header.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import "./Header.css";
 import ActiveQuestLogo from './ActiveQuest.png';
@@ -7,27 +8,31 @@ import MenuModal from './MenuModal.jsx';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => {
-    const [showMenu, setShowMenu] = useState(false);
-  
-    // Toggle Menu modal
-    const toggleMenu = () => {
-         setShowMenu(prev => !prev);
-    };
-  
-  
-    return (
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(prev => !prev);
+  };
+
+  return (
     <>
-      {/* Header bar */}
+      {/* Header Bar */}
       <div className="header">
-          <img  src={ActiveQuestLogo}   alt="ActiveQuest logo" className="header-logo" />
-        
-          <div className="icon-button" onClick={toggleMenu}>
-            <FontAwesomeIcon icon="user" />
-          </div>
+        <img src={ActiveQuestLogo} alt="ActiveQuest logo" className="header-logo" />
+
+        <div className="nav-buttons">
+          <Link to="/" className="nav-button">Activity</Link>
+          <Link to="/events" className="nav-button">Event</Link>
+          <Link to="/friends" className="nav-button">Friends</Link>
+        </div>
+
+        <div className="icon-button" onClick={toggleMenu}>
+          <FontAwesomeIcon icon="user" />
+        </div>
       </div>
 
       {/* Menu Modal */}
-      {showMenu && <MenuModal onClose={() => setShowMenu(false)}/>}
+      {showMenu && <MenuModal onClose={() => setShowMenu(false)} />}
     </>
   );
 };
