97484f6f854760f89064516831de9f098b42da6d
AndrazVZ <andraz.vz@gmail.com>
2025-06-02 16:16:31 +0200

    Added leaflet js maps with open street map tiles that now shows a path when activity card is expanded

--- Frontend-Web/src/ActivityCard.jsx
+++ Frontend-Web/src/ActivityCard.jsx
@@ -1,23 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './ActivityCard.css';
+import { MapContainer, TileLayer, Polyline } from 'react-leaflet';
 
 export default function ActivityCard({ activity }) {
   const [expanded, setExpanded] = useState(false);
+  
+
+  //console.log(activity);
+  const path = Array.isArray(activity.waypoints)
+  ? activity.waypoints.map(wp => [wp.lat, wp.lon])
+  : [];
 
   return (
     <div className="activity-card-container">
       <div className="activity-card" onClick={() => setExpanded(!expanded)}>
-        <div className="map-placeholder">
-          <strong>Waypoints / Map</strong>
-          <ul className="waypoint-list">
-            {activity.waypoints.map((wp, index) => (
-              <li key={index}>
-                lat: {wp.lat}, lon: {wp.lon}, alt: {wp.alt}
-              </li>
-            ))}
-          </ul>
-        </div>
-
+        
         <div className="activity-data">
           <p><strong>Start Time:</strong> {new Date(activity.startTime).toLocaleString()}</p>
           <p><strong>Duration:</strong> {activity.duration}</p>
@@ -26,9 +23,23 @@ export default function ActivityCard({ activity }) {
 
         {expanded && (
           <div className="expanded-section">
+            <MapContainer
+            center={path[0]}
+            zoom={20}
+            style={{height:'400px',width:'100%'}}
+            >
+            
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution="&copy; OpenStreetMap contributors"
+            />
+            <Polyline positions={path} color='blue'/>
+            </MapContainer>
+
             <p><strong>Avg Speed:</strong> {activity.avgSpeed ?? 'N/A'} km/h</p>
             <p><strong>Event ID:</strong> {activity.eventId}</p>
           </div>
+
         )}
       </div>
     </div>
