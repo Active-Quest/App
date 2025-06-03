1a9b7cf41e61ee810e06454657a6b4793ba8ea5a
MaticM4 <murko.matic4@gmail.com>
2025-06-03 16:36:20 +0200

    Added augmentation option for fliping the image

--- 2FA/libs/libs.py
+++ 2FA/libs/libs.py
@@ -3,7 +3,7 @@ import numpy as np
 import random as rng
 import math
 
-def rotate(slika,angle):
+def rotate(slika,angle=30):
     angle += np.random.randint(5, 10)
     h, w = slika.shape[:2]
     center = (w // 2, h // 2)
@@ -13,5 +13,14 @@ def rotate(slika,angle):
     rotated = cv.warpAffine(slika, M, (w, h),borderMode=cv.BORDER_REFLECT) # rotira slika za podan kot 
     #borderMode=cv.BORDER_REFLECT v kotah kjer nebi bilo "slike" zercali sosednje piksle
     return rotated
+
+def flip(slika , direction = "h"):
+    if direction == "h":
+        cv.flip(slika , 1) # obrne sliko horiziotalno
+    elif direction == "v":
+        cv.flip(slika , 0) # obrne sliko vertikalno
+    elif direction == "b":
+        cv.flip(slika , -1) # obrne sliko horiziotalno in vertikalno
+    return slika
 
 
