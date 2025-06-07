212b111619e6f81a8cd25d3b29c5681083cb208b
AndrazVZ <136996591+AndrazVZ@users.noreply.github.com>
2025-06-08 00:19:52 +0200

    Update userRoutes.js because of intercepting routes

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -2,26 +2,28 @@ var express = require('express');
 var router = express.Router();
 var userController = require('../controllers/userController.js');
 
-router.put('/:id/friends', userController.add);
-router.get('/:id/friends', userController.listFriends);
-router.get('/', userController.list);
 
 router.get('/me', userController.me);         
 router.get('/profile', userController.profile);
 router.get('/logout', userController.logout);
-
 router.post('/register', userController.create);
 router.post('/login', userController.login);
 router.post('/mobile-login', userController.mobileLogin);
+router.get('/search', userController.find);
+router.get('/checkFAStatus/:id',userController.check2FAStatus);
+
+
 router.post('/:id/update2FA',userController.update2FA);
 router.post('/:id/update2FAResult', userController.update2FAResult);
-router.get('/search', userController.find);
-router.get('/checkFAStatus/:id',userController.check2FAStatus)
-
+router.put('/:id/friends', userController.add);
+router.get('/:id/friends', userController.listFriends);
 router.put('/:id', userController.update);
 router.delete('/:id', userController.remove);
 
-router.get('/:id', userController.show);   
+
+router.get('/:id', userController.show); 
+
+  
 
 
 
