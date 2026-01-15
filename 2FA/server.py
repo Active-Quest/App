3b9a684d31c3b46ff87256c82768ef01edc296db
AndrazVZ <andraz.vz@gmail.com>
2026-01-15 18:53:13 +0100

    Added compression to 2FA files

--- 2FA/server.py
+++ 2FA/server.py
@@ -1,10 +1,16 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,Response
 import os
 import uuid
 from mpi4py import MPI
+import sys
 
 from register_user import register_user_logic
 from verify_user import verify_user_logic
+from Compress_images.compress_images import compress_flocic,decompress_flocic
+
+LOG_FILE = "/tmp/server.log"
+sys.stdout = open(LOG_FILE,"a",buffering=1)
+sys.stderr = open(LOG_FILE,"a",buffering=1)
 
 app = Flask("Pyserver")
 comm = MPI.COMM_WORLD
@@ -38,12 +44,23 @@ def register():
 
         dest_rank = (i % (size - 1)) + 1 
         print(f"[MASTER] Sending task {i} to worker {dest_rank}", flush=True)
+        #Shranim slike v tmp, in jih kompresiram z FLOCIC
+        img_path = f"/tmp/{user_id}_{uuid.uuid4().hex}.png"
+        compressed_img_path = f"/tmp/compressed/{user_id}_{i}.fcic";
+        with open(img_path, "wb") as f:
+            f.write(img_bytes)
+        #KOMPRESIJA SLIKE
+        compress_flocic(img_path,compressed_img_path)
+        original_img_size = os.path.getsize(img_path)
+        compressed_img_size = os.path.getsize(compressed_img_path)
+        print(f"[{user_id}_{i}.fcic] - {compressed_img_size}\n [ORIGINAL] - {img_path}")
 
+    
         comm.send(
             {
                 "cmd": "PROCESS",
                 "user_id": user_id,
-                "image": img_bytes,
+                "compressed_image_path": compressed_img_path,
             },
             dest=dest_rank,
         )
@@ -90,14 +107,16 @@ def worker_loop():
             break
 
         user_id = task["user_id"]
-        img_bytes = task["image"]
+        compressed_img_path = task["compressed_img_path"]
 
-        # unique temp file
-        img_path = f"/tmp/{user_id}_{rank}_{uuid.uuid4().hex}.jpg"
-        with open(img_path, "wb") as f:
-            f.write(img_bytes)
-
+        #unique temp file
+        #img_path = f"/tmp/{user_id}_{rank}_{uuid.uuid4().hex}.jpg"
+        #with open(img_path, "wb") as f:
+            #f.write(img_bytes)
         print(f"[WORKER {rank}] Processing image", flush=True)
+        img_path = f"/tmp/decompressed/{user_id}_{rank}_{uuid.uuid4().hex}.png"
+        #DEKOMPRESIJA FLOCIC 
+        decompress_flocic(compressed_img_path,img_path)
 
         try:
             success = register_user_logic(user_id, [img_path])
@@ -106,6 +125,7 @@ def worker_loop():
             success = False
 
         os.remove(img_path)
+        os.remove(compressed_img_path)
 
         comm.send(
             {
@@ -114,6 +134,15 @@ def worker_loop():
             },
             dest=0,
         )
+
+@app.route("/logs",methods=["GET"])
+def showLogs():
+    try:
+        with open("/tmp/server.log","r") as f:
+            content = f.read()
+            return Response(content,mimetype="text/plain")
+    except FileNotFoundError:
+        return "log file not found",404
 
 if __name__ == "__main__":
     if rank == 0:
